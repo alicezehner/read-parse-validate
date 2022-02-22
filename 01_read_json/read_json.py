a5e2daccf6d0392ae29f235b593dcfa44098daa4
alicezehner <alicezehner88@gmail.com>
2022-02-22 10:35:23 -0500

    update

--- 01_read_json/read_json.py
+++ 01_read_json/read_json.py
@@ -1,6 +1,9 @@
 # ---------------------------------------------
 # import package to read json
+
 # ---------------------------------------------
+# python3 read_json.py
+# import json
 
 
 # ---------------------------------------------
@@ -8,17 +11,20 @@
 # ---------------------------------------------
 
 def readJson(file):
-    pass
+    with open(file) as p:
+        return json.load(p)
 
 # ---------------------------------------------
 #  call 'readJson', load salaries
 # ---------------------------------------------
-
+salaries = readJson('./data.json')
+#print(salaries)
 
 # ---------------------------------------------
 #  print all salaries
 # ---------------------------------------------
-
+for salary in salaries['data']:
+    print(salary[18])
 
 # ---------------------------------------------
 # loop through list, only print salary field
--- 02_list_files/read_json_multiple.py
+++ 02_list_files/read_json_multiple.py
@@ -2,13 +2,29 @@
 # import package to read json
 # import package to walk file system
 # -------------------------------------
+# python3 read_json_multiple.py
 
+import json
+import glob
 
 # -----------------------------------
 #  list all files
 # -----------------------------------
+pattern = './data/*/*.json'
+data = []
 
+for file in glob.glob(pattern):
+    data.append(file)
 
+print(data)
 # -----------------------------------
 #  loop through files, parse json
 # -----------------------------------
+
+def readJson(file):
+    with open(file) as p:
+        return json.load(p)
+
+for file in data:
+    file = readJson(file)
+    print(file)
